09ef41bd4db072931b99ecf650283571772a75d1
Jordan <jordan@emporioht.com.br>
2015-11-17 14:41:56 -0200

    Ajuste da funcao para rodar no Windows

--- Gitblime.py
+++ Gitblime.py
@@ -1,5 +1,13 @@
 import sublime, sublime_plugin
 import subprocess 
+from subprocess import Popen, PIPE
+import os
+import re
+import sys
+
+
+if sublime.platform() != 'windows':
+	import pwd
 
 
 class GitblimeCommand(sublime_plugin.TextCommand):
@@ -8,8 +16,10 @@ class GitblimeCommand(sublime_plugin.TextCommand):
 
 	def run(self, edit):			
 		# self.showPainel( self.gitLogBunito() );
-		print( self.getTotalCommit() )
-		# print(self.gitStatus())
+		p = subprocess.Popen( "git status", cwd="C:/wamp/www/app/", shell=True, stdout=PIPE, stderr=PIPE)
+		stdout, stderr = p.communicate()
+		self.showPainel(str(stdout, encoding='utf8'))
+		
 
 	def showPainel( self, dados ):
 		self.window = self.view.window()
@@ -22,7 +32,7 @@ class GitblimeCommand(sublime_plugin.TextCommand):
 		self.window.run_command("show_panel", {"panel": "output.consulta"})
 
 	def gitStatus(self):
-		self.r = subprocess.check_output(["git", "status", "-s"], stderr=subprocess.STDOUT)
+		self.r = subprocess.check_output( "git status" , cwd="C:/wamp/www/app/", shell = True)
 		return str(self.r, encoding='utf8')
 
 	def gitLogBunito(self):
@@ -30,6 +40,17 @@ class GitblimeCommand(sublime_plugin.TextCommand):
 		return str(self.r, encoding='utf8')
 
 	def getTotalCommit(self):		
-		self.r = subprocess.check_output(["git", "log", "--pretty=format:'commit=%H;"])
+		self.r = subprocess.check_output("cmd git log ", shell=True)
 		retorno = str(self.r, encoding='utf8').split(';')
 		return len(retorno)
+	
+	def get_subl_executable_path( self ):
+		"""Return the path to the subl command line binary."""
+		executable_path = sublime.executable_path()
+
+		if sublime.platform() == 'osx':
+			suffix = '.app/'
+			app_path = executable_path[:executable_path.rfind(suffix) + len(suffix)]
+			executable_path = app_path + 'Contents/SharedSupport/bin/subl'
+
+		return executable_path
